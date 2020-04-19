f36e3692ba6a3be8510057c8710b071fcca3157d
WardZhouA <wardzhou@outlook.com>
2020-04-20 00:22:46 +0800

    Site updated: 2020-04-20 00:22:45

--- custom/typer.js
+++ custom/typer.js
@@ -1,8 +1,8 @@
 
-window.onload = function () {
+//window.onload = function () 
+setTimeout(function (){
     var $typeContent = $("[class='φee']");
     $typeContent.append('<div style="height:2.83rem; display:inline;"><span id="typer"></span></div>');
-    //, "Make time for civilization"," for civilization won't make time.", "Keep on sailing."
     var options = {
         strings: ["给文明以岁月","而不是给岁月以文明", "Make time for civilization"," for civilization won't make time."],
         typeSpeed: 50,
@@ -14,4 +14,4 @@ window.onload = function () {
         backDelay: 1000
     };
     var typed = new Typed("#typer", options);
-}
+},1300);
