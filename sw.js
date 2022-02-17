cf72f3ac2d9a9c8c28ab73016552b2adcb47d374
birch <33411000+derbirch@users.noreply.github.com>
2022-02-18 00:00:15 +0800

    Site updated: 2022-02-18 00:00:14

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@ workbox.setConfig({ modulePathPrefix: 'https://g.alicdn.com/kg/workbox/3.3.0/' }
 self.addEventListener('install', function (event) {
   event.waitUntil(
     caches.keys().then(function (names) {
-      var validSets = ["is-sw-27fa67","is-html-27fa67","is-jsdelivr-27fa67","is-gtm-27fa67","is-gravatar-27fa67","is-theme-27fa67","is-cdn-27fa67","is-json-27fa67","is-custom-27fa67","is-img-27fa67"];
+      var validSets = ["is-sw-9effe1","is-html-9effe1","is-jsdelivr-9effe1","is-gtm-9effe1","is-gravatar-9effe1","is-theme-9effe1","is-cdn-9effe1","is-json-9effe1","is-custom-9effe1","is-img-9effe1"];
       return Promise.all(
         names
           .filter(function (name) { return !~validSets.indexOf(name); })
@@ -18,38 +18,38 @@ self.addEventListener('install', function (event) {
 });
 
 workbox.routing.registerRoute(new RegExp('sw\\.js'), workbox.strategies.networkOnly({
-  cacheName: 'is-sw-27fa67',
+  cacheName: 'is-sw-9effe1',
 }));
 workbox.routing.registerRoute(new RegExp('https://cdn\\.jsdelivr\\.net'), workbox.strategies.staleWhileRevalidate({
-  cacheName: 'is-jsdelivr-27fa67',
+  cacheName: 'is-jsdelivr-9effe1',
   plugins: [ new workbox.expiration.Plugin({ maxAgeSeconds: 14400 }) ],
 }));
 workbox.routing.registerRoute(new RegExp('https://www\\.googletagmanager\\.com\?id=.*'), workbox.strategies.staleWhileRevalidate({
-  cacheName: 'is-gtm-27fa67',
+  cacheName: 'is-gtm-9effe1',
   plugins: [ new workbox.expiration.Plugin({ maxAgeSeconds: 14400 }) ],
 }));
 workbox.routing.registerRoute(new RegExp('https://www\\.gravatar\\.com'), workbox.strategies.staleWhileRevalidate({
-  cacheName: 'is-gravatar-27fa67',
+  cacheName: 'is-gravatar-9effe1',
   plugins: [ new workbox.expiration.Plugin({ maxAgeSeconds: 14400 }) ],
 }));
 workbox.routing.registerRoute(new RegExp('/.*\\.(?:js|css|woff2|png|jpg|gif)$'), workbox.strategies.staleWhileRevalidate({
-  cacheName: 'is-theme-27fa67',
+  cacheName: 'is-theme-9effe1',
   plugins: [ new workbox.expiration.Plugin({ maxAgeSeconds: 14400 }) ],
 }));
 workbox.routing.registerRoute(new RegExp('https://cdn\\.wardzhou\\.art'), workbox.strategies.staleWhileRevalidate({
-  cacheName: 'is-cdn-27fa67',
+  cacheName: 'is-cdn-9effe1',
   plugins: [ new workbox.expiration.Plugin({ maxAgeSeconds: 14400 }) ],
 }));
 workbox.routing.registerRoute(new RegExp('your_data_prefix/.*\\.json'), workbox.strategies.cacheFirst({
-  cacheName: 'is-json-27fa67',
+  cacheName: 'is-json-9effe1',
   plugins: [ new workbox.expiration.Plugin({ maxAgeSeconds: 14400 }) ],
 }));
 workbox.routing.registerRoute(new RegExp('/custom/.*'), workbox.strategies.staleWhileRevalidate({
-  cacheName: 'is-custom-27fa67',
+  cacheName: 'is-custom-9effe1',
   plugins: [ new workbox.expiration.Plugin({ maxAgeSeconds: 14400 }) ],
 }));
 workbox.routing.registerRoute(new RegExp('/img/.*'), workbox.strategies.staleWhileRevalidate({
-  cacheName: 'is-img-27fa67',
+  cacheName: 'is-img-9effe1',
   plugins: [ new workbox.expiration.Plugin({ maxAgeSeconds: 14400 }) ],
 }));
 
