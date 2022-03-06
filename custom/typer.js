8735e958b395a3dfa01bf423f63ba4242c2bb3a9
birch <33411000+derbirch@users.noreply.github.com>
2022-03-06 22:47:40 +0800

    Site updated: 2022-03-06 22:47:39

--- custom/typer.js
+++ custom/typer.js
@@ -4,7 +4,7 @@ setTimeout(function() {
     $typeContent.append('<span id="typer"></span>');
     $typeContent.attr("style", "height:2.83rem; display:block;");
     var options = {
-        strings: ["风吹过白桦林，将未完的故事娓娓道来。", "Wind comes to the birch,", "telling the unfinished stories."],
+        strings: ["战火踩过白桦林，将未完的故事悉数尽焚。", "War comes to the birch,", "wiping the unfinished stories."],
         typeSpeed: 50,
         contentType: 'html',
         loop: true,
